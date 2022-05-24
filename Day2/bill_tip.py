5f083e5b3b930e2bd8b32c7e2a66556093b45660
Sase <sase@sase>
2022-05-24 10:29:31 +0100

    bill_tip in Day2 and new folder for Day3

---
+++ Day2/bill_tip.py
@@ -0,0 +1,27 @@
+print('welcome to the tip calculator')
+
+bill = float(input('What was to total bill? $'))
+tip = int(input('How much tip percentage would you like to give? 10, 12 or 15? '))
+people = int(input('How many people will split the bill ? '))
+
+tip_cal = tip / 100 * bill
+calc = tip / 100 * bill + bill
+
+indiv = calc / people
+
+print(f'This is the tip {tip_cal}')
+
+print(f'This is the bill including the tip {calc}')
+
+print(f'This is the share including the tip {indiv}')
+
+
+# All the calculations in one line
+
+fina = (tip / 100 * bill + bill) / people
+
+# Formating to show 2 decimals
+fina = "{:.2f}".format(fina)
+
+
+print(f"Each person should pay :  ${fina} ")
