a015b66f2cb0e2c7fa55c82e58518b888c167357
barrowhall <sase@sase>
2022-05-26 16:25:07 +0100

    Changed rock_simple.py

--- Day4/rock_simple.py
+++ Day4/rock_simple.py
@@ -34,44 +34,68 @@ print("Let's play Rock-Paper-Scissors !!!!")
 
 mas = int(input('What do you choose? Type 0 for Rock, 1 for Paper and 2 for Scissors: \n '))
 
-aiur = random.randint(0, 2)
 
-comp = lista[aiur]
-tu = lista[mas]
-
-if mas == aiur:
-	print(comp)
-	print('Computer chose.')
-	print(tu)
-	print('It is a tie ===')
-
-elif mas == 0 and aiur == 2:
-	print(comp)
-	print('Computer chose.')
-	print(tu)
-	print('You Win :((')
+if mas >= 3 or mas < 0:
+	print('Not a valid choice, You Lose :((')
 	
-elif mas == 2 and aiur == 0:
-	print(comp)
-	print('Computer chose.')
-	print(tu)
-	print('You Lose :((')
-
-elif mas > aiur:
-	print(comp)
-	print('Computer chose.')
-	print(tu)
-	print('You Lose :((')
-
-elif mas < aiur:
-	print(comp)
-	print('Computer chose.')
-	print(tu)
-	print('You Lose :((')
-
 else:
-	print('Not a choice , You Lose :((')
 
-
-
+	aiur = random.randint(0, 2)
+	comp = lista[aiur]
+	tu = lista[mas]
+	
+	sign = ""
+	signu = ""
+	
+	if aiur == 0:
+		sign = 'Rock'
+	elif aiur== 1:
+		sign = 'Paper'
+	elif aiur == 2:
+		sign = 'Scissors'
+		
+	if mas == 0:
+		signu = 'Rock'
+	elif mas == 1:
+		signu = 'Paper'
+	elif mas == 2:
+		signu = 'Scissors'	
+		
+		
+	if mas == aiur:
+		print(f'You choose {signu}.')
+		print(tu)
+		print(f'Computer chose {sign}.')
+		print(comp)
+		print('It is a tie ===')
+	
+	elif mas == 0 and aiur == 2:
+		print(f'You choose {signu}.')
+		print(tu)
+		print(f'Computer chose {sign}.')
+		print(comp)
+		print('You Win :((')
+		
+	elif mas == 2 and aiur == 0:
+		print(f'You choose {signu}.')
+		print(tu)
+		print(f'Computer chose {sign}.')
+		print(comp)
+		print('You Lose :((')
+	
+	elif mas > aiur:
+		print(f'You choose {signu}.')
+		print(tu)
+		print(f'Computer chose {sign}.')
+		print(comp)
+		print('You Lose :((')
+	
+	elif mas < aiur:
+		print(f'You choose {signu}.')
+		print(tu)
+		print(f'Computer chose {sign}.')
+		print(comp)
+		print('You Lose :((')
+	
+	
 
